27925474e47ac46de407e04fba510be38e20d05c
APeev89 <apostol.peev@mail.bg>
2022-10-04 15:27:39 +0300

    Initial commit

---
+++ MarketArea/MarketArea/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MarketArea.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
