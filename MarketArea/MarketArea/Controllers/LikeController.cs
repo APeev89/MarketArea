95507a9485ec831778b237571d581b01a90a5261
APeev89 <apostol.peev@mail.bg>
2022-11-21 17:18:13 +0200

    Create LikeController

---
+++ MarketArea/MarketArea/Controllers/LikeController.cs
@@ -0,0 +1,29 @@
+﻿using MarketArea.Data.Common;
+using MarketArea.Data.ModelDb;
+using MarketArea.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketArea.Controllers
+{
+    public class LikeController : BaseController
+    {
+        private readonly IRepository repo;
+        private readonly UserManager<IdentityUser> userManager;
+        public LikeController(IRepository _repo, UserManager<IdentityUser> _userManager)
+        {
+            repo = _repo;
+            userManager = _userManager;
+        }
+
+        public IActionResult LikeDislike(string id)
+        {
+
+
+            return Json(new { result = "success" });
+        }
+
+       
+    }
+}
