842d3815d17374ec5871f6c17463e7a420636063
APeev89 <apostol.peev@mail.bg>
2022-11-21 21:54:14 +0200

    Creating SeenController

---
+++ MarketArea/MarketArea/Controllers/SeenController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketArea.Controllers
+{
+    public class SeenController : BaseController
+    {
+
+        public SeenController()
+        {
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Seen(string id)
+        {
+            return Json(new { result = "success" });
+        }
+    }
+}
