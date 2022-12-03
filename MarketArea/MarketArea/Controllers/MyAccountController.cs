f543f62dbaa5529e84a62c18812a6d3a3cd9cf30
APeev89 <apostol.peev@mail.bg>
2022-12-03 16:13:44 +0200

    Making of MyAccountController with action MyAdsList

---
+++ MarketArea/MarketArea/Controllers/MyAccountController.cs
@@ -0,0 +1,42 @@
+﻿using MarketArea.Data.Common;
+using MarketArea.Data.ModelDb;
+using MarketArea.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketArea.Controllers
+{
+    public class MyAccountController : BaseController
+    {
+        private readonly IRepository repo;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public MyAccountController(IRepository _repo, UserManager<IdentityUser> _userManager)
+        {
+            repo = _repo;
+            userManager = _userManager;
+        }
+
+        [Authorize]
+        public IActionResult MyAdsList()
+        {
+            IEnumerable<Ad>? myAdsList = null;
+            var user = userManager.GetUserAsync(User).Result;
+            var myAds = repo.All<Ad>().Where(x => x.UserId == user.Id)
+                                      .Include(a => a.City)
+                                      .OrderBy(x => x.DateFrom);
+
+            if (myAds.Count() != 0)
+            {
+                myAdsList = myAds;
+            }
+
+            return View(new MyAdsViewModel
+            {
+                MyAdsList = myAdsList,
+            });
+        }
+    }
+}
---
+++ MarketArea/MarketArea/ViewModels/MyAdsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using MarketArea.Data.ModelDb;
+
+namespace MarketArea.ViewModels
+{
+    public class MyAdsViewModel
+    {
+        public IEnumerable<Ad>? MyAdsList { get; set; }
+    }
+}
